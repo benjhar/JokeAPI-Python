a8f75b8dff249a4eedd7a887e569af1c1cd29a09
thenamesweretakenalready <43702423+thenamesweretakenalready@users.noreply.github.com>
2020-02-29 23:54:50 +0000

    im a dumb bitch

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 setup(
   name='jokeapi',
   packages=['jokeapi'],   # Chose the same as "name"
-  version='0.1',
+  version='0.1.1',
   license='GNU General Public License v3 (GPLv3)',
   description='An API Wrapper for Sv443\'s JokeAPI',
   author='thenamesweretakenalready',
   author_email='leet_haker@cyber-wizard.com',
   url="""https://github.com/
   thenamesweretakenalready/Sv443s-JokeAPI-Python-Wrapper""",
-  download_url='https://github.com/user/reponame/archive/v0.1.tar.gz',
+  download_url='https://github.com/user/Sv443s-JokeAPI-Python-Wrapper/archive/v0.1.1.tar.gz',
   keywords=['api wrapper', 'wrapper', 'api', 'jokes'],
   install_requires=[],
   classifiers=[
