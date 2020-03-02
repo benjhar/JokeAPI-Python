d30425e34064e920fcd1b1034ec554a0dabe5a60
thenamesweretakenalready <43702423+thenamesweretakenalready@users.noreply.github.com>
2020-03-02 07:26:33 +0000

    Add README to PyPi page

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
-from distutils.core import setup
-setup(
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
   name='jokeapi',
   packages=['jokeapi'],
-  version='0.1.2',
+  version='0.1.3',
   license='GNU General Public License v3 (GPLv3)',
   description='An API Wrapper for Sv443\'s JokeAPI',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
   author='thenamesweretakenalready',
   author_email='leet_haker@cyber-wizard.com',
   url="""https://github.com/thenamesweretakenalready/Sv443s-JokeAPI-Python-Wrapper""",
-  download_url='https://github.com/user/Sv443s-JokeAPI-Python-Wrapper/archive/v0.1.2.tar.gz',
+  download_url='https://github.com/user/Sv443s-JokeAPI-Python-Wrapper/archive/v0.1.3.tar.gz',
   keywords=['api wrapper', 'wrapper', 'api', 'jokes'],
-  install_requires=[],
+  install_requires=setuptools.find_packages(),
   classifiers=[
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
