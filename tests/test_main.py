db8bc1b0fd70dbc9408cec1a4b79960e9e04d294
thenamesweretakenalready <leet_haker@cyber-wizard.com>
2021-08-05 17:38:25 +0100

    :green_heart: Use user_agent in all test cases

--- tests/test_main.py
+++ tests/test_main.py
@@ -18,82 +18,82 @@ async def test_main():
     token = getenv("token")
 
     try:
-        await j.get_joke()
+        await j.get_joke(user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/91.0.4472.164 Safari/537.36")
     except Exception as e:
         errors.append({"Error in": "blank joke get", "Error": e})
 
     """Testing auth tokens"""
     try:
-        await j.get_joke(auth_token=token)
+        await j.get_joke(auth_token=token, user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/91.0.4472.164 Safari/537.36")
     except Exception as e:
         auth_token = None
         errors.append({"Error in": "auth usage", "Error": e})
 
     """Testing for errors in categories"""
     try:
-        await j.get_joke(category=["programming"], auth_token=token)
+        await j.get_joke(category=["programming"], auth_token=token, user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/91.0.4472.164 Safari/537.36")
     except Exception as e:
         errors.append({"Error in": "category programming", "Error": e})
     try:
-        await j.get_joke(category=["misc"], auth_token=token)
+        await j.get_joke(category=["misc"], auth_token=token, user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/91.0.4472.164 Safari/537.36")
     except Exception as e:
         errors.append({"Error in": "category miscellaneous", "Error": e})
     try:
-        await j.get_joke(category=["dark"], auth_token=token)
+        await j.get_joke(category=["dark"], auth_token=token, user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/91.0.4472.164 Safari/537.36")
     except Exception as e:
         errors.append({"Error in": "category dark", "Error": e})
 
     """Testing for errors in blacklist"""
     try:
-        await j.get_joke(blacklist=["nsfw"], auth_token=token)
+        await j.get_joke(blacklist=["nsfw"], auth_token=token, user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/91.0.4472.164 Safari/537.36")
     except Exception as e:
         errors.append({"Error in": "blacklist nsfw", "Error": e})
 
     try:
-        await j.get_joke(blacklist=["religious"], auth_token=token)
+        await j.get_joke(blacklist=["religious"], auth_token=token, user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/91.0.4472.164 Safari/537.36")
     except Exception as e:
         errors.append({"Error in": "blacklist religious", "Error": e})
 
     try:
-        await j.get_joke(blacklist=["political"], auth_token=token)
+        await j.get_joke(blacklist=["political"], auth_token=token, user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/91.0.4472.164 Safari/537.36")
     except Exception as e:
         errors.append({"Error in": "blacklist political", "Error": e})
 
     try:
-        await j.get_joke(blacklist=["racist"], auth_token=token)
+        await j.get_joke(blacklist=["racist"], auth_token=token, user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/91.0.4472.164 Safari/537.36")
     except Exception as e:
         errors.append({"Error in": "blacklist political", "Error": e})
 
     try:
-        await j.get_joke(blacklist=["sexist"], auth_token=token)
+        await j.get_joke(blacklist=["sexist"], auth_token=token, user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/91.0.4472.164 Safari/537.36")
     except Exception as e:
         errors.append({"Error in": "blacklist sexist", "Error": e})
 
     """Testing for errors in response_format"""
     try:
-        await j.get_joke(response_format="xml", auth_token=token)
+        await j.get_joke(response_format="xml", auth_token=token, user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/91.0.4472.164 Safari/537.36")
     except Exception as e:
         errors.append({"Error in": "response_format xml", "Error": e})
 
     try:
-        await j.get_joke(response_format="yaml", auth_token=token)
+        await j.get_joke(response_format="yaml", auth_token=token, user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/91.0.4472.164 Safari/537.36")
     except Exception as e:
         errors.append({"Error in": "response_format yaml", "Error": e})
 
     """Testing for errors in type"""
     try:
-        await j.get_joke(joke_type="single", auth_token=token)
+        await j.get_joke(joke_type="single", auth_token=token, user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/91.0.4472.164 Safari/537.36")
     except Exception as e:
         errors.append({"Error in": "type single", "Error": e})
 
     try:
-        await j.get_joke(joke_type="twopart", auth_token=token)
+        await j.get_joke(joke_type="twopart", auth_token=token, user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/91.0.4472.164 Safari/537.36")
     except Exception as e:
         errors.append({"Error in": "type double", "Error": e})
 
     """Testing for errors in search_string"""
     try:
-        await j.get_joke(search_string="search", auth_token=token)
+        await j.get_joke(search_string="search", auth_token=token, user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/91.0.4472.164 Safari/537.36")
         # as long as this gets a response, the api wrapper is fine;
         # it probably doesn't exist in a joke.
     except Exception as e:
@@ -101,13 +101,13 @@ async def test_main():
 
     """Testing for errors in id_range"""
     try:
-        await j.get_joke(id_range=[30, 151], auth_token=token)
+        await j.get_joke(id_range=[30, 151], auth_token=token, user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/91.0.4472.164 Safari/537.36")
     except Exception as e:
         errors.append({"Error in": "id_range", "Error": e})
 
     """Testing for errors in safe_mode"""
     try:
-        await j.get_joke(safe_mode=True, auth_token=token)
+        await j.get_joke(safe_mode=True, auth_token=token, user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/91.0.4472.164 Safari/537.36")
     except Exception as e:
         errors.append({"Error in": "safe_mode", "Error": e})
 
